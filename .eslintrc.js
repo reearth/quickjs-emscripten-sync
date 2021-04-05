666d8f89c3235301be7fbace9efced89b8ad265c
rot1024 <aayhrot@gmail.com>
2021-04-05 13:22:16 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "plugin:prettier/recommended"
+  ],
+  "settings": {
+    "react": {
+      "version": "999.999.999"
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# quickjs-emscripten-sync
+
+This library wraps quickjs-emscripten and provides a way to sync object state between browser and sandboxed QuickJS.
