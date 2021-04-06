3b751035bc7b6c505418a0d69a5ff13ecffefcdb
rot1024 <aayhrot@gmail.com>
2021-04-06 18:31:26 +0900

    impl marshal/properties

---
+++ src/marshal/properties.test.ts
@@ -0,0 +1,77 @@
+import { getQuickJS, QuickJSHandle } from "quickjs-emscripten";
+import marshalProperties from "./properties";
+
+it("works", async () => {
+  const vm = (await getQuickJS()).createVm();
+  const descTester = vm.unwrapResult(
+    vm.evalCode(`(obj, expected) => {
+      const descs = Object.getOwnPropertyDescriptors(obj);
+      for (const [k, v] of Object.entries(expected)) {
+        const d = descs[k];
+        if (v.valueType && typeof d.value !== v.valueType) throw new Error(k + " value invalid");
+        if (v.getType && typeof d.get !== v.getType) throw new Error(k + " get invalid");
+        if (v.setType && typeof d.set !== v.setType) throw new Error(k + " set invalid");
+        if (typeof v.enumerable === "boolean" && d.enumerable !== v.enumerable) throw new Error(k + " enumerable invalid: " + d.enumerable);
+        if (typeof v.configurable === "boolean" && d.configurable !== v.configurable) throw new Error(k + " configurable invalid: " + d.configurable);
+        if (typeof v.writable === "boolean" && d.writable !== v.writable) throw new Error(k + " writable invalid: " + d.writable);
+      }
+    }`)
+  );
+
+  const disposables: QuickJSHandle[] = [];
+  const marshaler = jest.fn(() => {
+    const fn = vm.newFunction("", () => {});
+    disposables.push(fn);
+    return fn;
+  });
+
+  const handle = vm.newObject();
+  const obj = {};
+  const bar = () => {};
+  const fooGet = () => {};
+  const fooSet = () => {};
+  Object.defineProperties(obj, {
+    bar: {
+      value: bar,
+      enumerable: true,
+      configurable: true,
+      writable: true,
+    },
+    foo: {
+      get: fooGet,
+      set: fooSet,
+      enumerable: false,
+      configurable: true,
+    },
+  });
+
+  marshalProperties(vm, obj, handle, marshaler);
+  expect(marshaler.mock.calls).toEqual([[bar], [fooGet], [fooSet]]);
+
+  const expected = vm.unwrapResult(
+    vm.evalCode(`({
+      bar: { valueType: "function", getType: "undefined", setType: "undefined", enumerable: true, configurable: true, writable: true },
+      foo: { valueType: "undefined", getType: "function", setType: "function", enumerable: false, configurable: true }
+    })`)
+  );
+  vm.unwrapResult(vm.callFunction(descTester, vm.undefined, handle, expected));
+
+  expected.dispose();
+  disposables.forEach(d => d.dispose());
+  handle.dispose();
+  descTester.dispose();
+  vm.dispose();
+});
+
+it("empty", async () => {
+  const vm = (await getQuickJS()).createVm();
+  const marshaler = jest.fn();
+  const handle = vm.newObject();
+  const obj = {};
+
+  marshalProperties(vm, obj, handle, marshaler);
+  expect(marshaler).toHaveBeenCalledTimes(0);
+
+  handle.dispose();
+  vm.dispose();
+});
---
+++ src/marshal/properties.ts
@@ -0,0 +1,50 @@
+import { QuickJSVm, QuickJSHandle } from "quickjs-emscripten";
+
+export default function marshalProperties(
+  vm: QuickJSVm,
+  target: object | Function,
+  handle: QuickJSHandle,
+  marshaler: (target: any) => QuickJSHandle
+): void {
+  const descs = vm.newObject();
+
+  Object.entries(Object.getOwnPropertyDescriptors(target)).forEach(
+    ([key, desc]) => {
+      const valueHandle =
+        typeof desc.value === "undefined" ? undefined : marshaler(desc.value);
+      const getHandle =
+        typeof desc.get === "undefined" ? undefined : marshaler(desc.get);
+      const setHandle =
+        typeof desc.set === "undefined" ? undefined : marshaler(desc.set);
+
+      vm.newObject().consume(descObj => {
+        Object.entries(desc).forEach(([k, v]) => {
+          const v2 =
+            k === "value"
+              ? valueHandle
+              : k === "get"
+              ? getHandle
+              : k === "set"
+              ? setHandle
+              : v
+              ? vm.true
+              : vm.false;
+          if (v2) {
+            vm.setProp(descObj, k, v2);
+          }
+        });
+        vm.setProp(descs, key, descObj);
+      });
+    }
+  );
+
+  vm.unwrapResult(vm.evalCode(`Object.defineProperties`)).consume(
+    defineProperties => {
+      vm.unwrapResult(
+        vm.callFunction(defineProperties, vm.undefined, handle, descs)
+      ).dispose();
+    }
+  );
+
+  descs.dispose();
+}
