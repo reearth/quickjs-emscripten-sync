bc6adbf0af386cbc1db56119ed567ba65aa9dd6d
rot1024 <aayhrot@gmail.com>
2021-04-09 20:53:01 +0900

    impl index

---
+++ src/index.test.ts
@@ -0,0 +1,86 @@
+import { getQuickJS } from "quickjs-emscripten";
+import Arena from ".";
+
+it("evalCode", async () => {
+  const vm = (await getQuickJS()).createVm();
+  const arena = new Arena(vm);
+
+  const result = arena.evalCode(`({ a: 1, b: a => Math.floor(a) })`);
+  expect(result).toEqual({ a: 1, b: expect.any(Function) });
+  expect(result.b(1.1)).toBe(1);
+
+  arena.dispose();
+  vm.dispose();
+});
+
+it("expose", async () => {
+  const vm = (await getQuickJS()).createVm();
+  const arena = new Arena(vm);
+
+  const obj = {
+    a: 1,
+    b: (a: number) => Math.floor(a),
+    c() {
+      return this.a++;
+    },
+  };
+  const obj2 = arena.expose({ obj });
+
+  expect(arena.evalCode(`obj.b(1.1)`)).toBe(1);
+  expect(arena.evalCode(`obj.c()`)).toBe(1);
+  expect(arena.evalCode(`obj.a`)).toBe(2);
+  expect(obj.a).toBe(2);
+  expect(arena.evalCode(`obj.c()`)).toBe(2);
+  expect(arena.evalCode(`obj.a`)).toBe(3);
+  expect(obj.a).toBe(3);
+  expect(arena.evalCode(`obj.c()`)).toBe(3);
+  expect(arena.evalCode(`obj.a`)).toBe(4);
+  expect(obj.a).toBe(4);
+
+  expect(obj).toBe(obj2.obj);
+  obj2.obj.a = 10;
+  expect(arena.evalCode(`obj.a`)).toBe(4); // not affected
+
+  arena.evalCode(`obj.a = 100`);
+  expect(obj.a).toBe(10); // not affected
+  expect(obj2.obj.a).toBe(10); // not affected
+
+  arena.dispose();
+  vm.dispose();
+});
+
+it("expose with sync", async () => {
+  const vm = (await getQuickJS()).createVm();
+  const arena = new Arena(vm);
+
+  const obj = {
+    a: 1,
+    b: (a: number) => Math.floor(a),
+    c() {
+      return this.a++;
+    },
+  };
+  const obj2 = arena.expose({ obj }, true);
+
+  expect(arena.evalCode(`obj.b(1.1)`)).toBe(1);
+  expect(arena.evalCode(`obj.c()`)).toBe(1);
+  expect(arena.evalCode(`obj.a`)).toBe(2);
+  expect(obj.a).toBe(2);
+  expect(arena.evalCode(`obj.c()`)).toBe(2);
+  expect(arena.evalCode(`obj.a`)).toBe(3);
+  expect(obj.a).toBe(3);
+  expect(arena.evalCode(`obj.c()`)).toBe(3);
+  expect(arena.evalCode(`obj.a`)).toBe(4);
+  expect(obj.a).toBe(4);
+
+  expect(obj).not.toBe(obj2.obj);
+  obj2.obj.a = 10;
+  expect(arena.evalCode(`obj.a`)).toBe(10); // affected
+
+  arena.evalCode(`obj.a = 100`);
+  expect(obj.a).toBe(100); // affected
+  expect(obj2.obj.a).toBe(100); // affected
+
+  arena.dispose();
+  vm.dispose();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,167 @@
-export const sum = (a: number, b: number) => {
-  if ("development" === process.env.NODE_ENV) {
-    console.log("boop");
-  }
-  return a + b;
+import { QuickJSHandle, QuickJSVm } from "quickjs-emscripten";
+import VMMap from "./vmmap";
+import VMSet from "./vmset";
+import marshal from "./marshal";
+import unmarshal from "./unmarshal";
+import { complexity, isES2015Class, isObject } from "./util";
+
+export {
+  Arena,
+  VMMap,
+  VMSet,
+  marshal,
+  unmarshal,
+  complexity,
+  isES2015Class,
+  isObject,
 };
+
+export default class Arena {
+  vm: QuickJSVm;
+  _map: VMMap;
+  _sync: VMSet;
+  _symbol: QuickJSHandle;
+  _symbol2 = Symbol();
+  _setterHandle: QuickJSHandle;
+  _syncHandle: QuickJSHandle;
+
+  constructor(vm: QuickJSVm) {
+    this.vm = vm;
+    this._symbol = vm.unwrapResult(vm.evalCode(`Symbol()`));
+    this._map = new VMMap(vm, this._symbol);
+    this._sync = new VMSet(vm, this._symbol);
+    this._setterHandle = vm.newFunction("set", (robj, rkey, rvalue) => {
+      const obj = this._map.getByHandle(robj);
+      if (typeof obj === "undefined") return vm.false;
+
+      const key = this._unmarshal(rkey);
+      if (typeof key !== "string") return vm.false; // symbol is not supported
+
+      const value = this._unmarshal(rvalue);
+      return Reflect.set(obj, key, value) ? vm.true : vm.false;
+    });
+    this._syncHandle = vm.unwrapResult(
+      vm.evalCode(`(name, object, setter, proxyTarget) => {
+        const handler = {
+          get(obj, key) {
+            if (typeof proxyTarget === "symbol" && key === proxyTarget) {
+              return Reflect.get(obj, proxyTarget) ?? obj;
+            }
+            const target = Reflect.get(obj, key);
+            if (typeof target === "function" || (typeof target === "object" && target !== null)) {
+              return new Proxy(target, handler);
+            }
+            return target;
+          },
+          set(obj, key, value) {
+            return Reflect.set(obj, key, value) && setter(obj, key, value);
+          }
+        };
+        globalThis[name] = new Proxy(object, handler);
+      }`)
+    );
+  }
+
+  evalCode(code: string) {
+    const result = this.vm.evalCode(code);
+    if (!result) return undefined;
+    if ("value" in result) {
+      return result.value.consume(v => this._unmarshal(v));
+    }
+    throw result.error.consume(err => this._unmarshal(err));
+  }
+
+  expose<T extends { [k: string]: any }>(obj: T, sync?: boolean): T {
+    const newobject = Object.entries(obj).map(([key, value]) => {
+      const isSyncable = sync && isObject(value);
+      const handle = this._marshal(value, isSyncable);
+
+      if (isSyncable) {
+        this.vm.newString(key).consume(n => {
+          this.vm.unwrapResult(
+            this.vm.callFunction(
+              this._syncHandle,
+              this.vm.undefined,
+              n,
+              handle,
+              this._setterHandle,
+              this._symbol
+            )
+          );
+        });
+      } else {
+        this.vm.setProp(this.vm.global, key, handle);
+      }
+
+      return [
+        key,
+        isSyncable ? new Proxy(value, this._proxyHandler) : value,
+      ] as const;
+    });
+
+    return Object.fromEntries(newobject) as T;
+  }
+
+  _unwrap(value: any): any {
+    return isObject(value) ? (value as any)[this._symbol2] ?? value : value;
+  }
+
+  _proxyHandler: ProxyHandler<any> = {
+    get: (o, key) => {
+      if (key === this._symbol2) {
+        return o;
+      }
+      const v = Reflect.get(o, key);
+      return isObject(v) ? new Proxy(v, this._proxyHandler) : v;
+    },
+    set: (o, key, value) => {
+      const v = this._unwrap(value);
+      if (Reflect.set(o, key, v)) {
+        const o2 = this._map.get(o);
+        if (o2) {
+          this.vm.setProp(o2, key as any, this._marshal(v));
+        }
+        return true;
+      }
+      return false;
+    },
+  };
+
+  _marshal(target: any, sync?: boolean) {
+    const map = new VMMap(this.vm);
+    map.merge(this._map);
+    const d = marshal(target, {
+      vm: this.vm,
+      map,
+      unmarshaler: v => this._unmarshal(v, true),
+      proxyKeySymbol: this._symbol,
+    });
+    this._map.merge(map);
+    if (sync && isObject(target)) {
+      this._sync.add(...map.values());
+    }
+    map.clear();
+    map.dispose();
+    return d;
+  }
+
+  _unmarshal(handle: QuickJSHandle, sync?: boolean) {
+    const result = unmarshal(this.vm, handle, this._map, v =>
+      this._marshal(v, sync)
+    );
+
+    if ((sync && isObject(result)) || this._sync.has(result)) {
+      return new Proxy(result, this._proxyHandler);
+    }
+
+    return result;
+  }
+
+  dispose() {
+    this._sync.dispose();
+    this._map.dispose();
+    this._setterHandle.dispose();
+    this._syncHandle.dispose();
+    this._symbol.dispose();
+  }
+}
